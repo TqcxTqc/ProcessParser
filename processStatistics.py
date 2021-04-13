a3958c011ebc871ac7c39e973a1a63f297c88308
tqcxtqc <vitali.dambajev@insly.com>
2021-04-13 13:34:59 +0300

    Added txt file with results of parser and polished code

--- processStatistics.py
+++ processStatistics.py
@@ -1,29 +1,10 @@
 import subprocess
-import pprint
-import json
-from collections import defaultdict, Counter
-
-# REPORT keys
-USER = "users"
-USERS_PROCESS_COUNT = "users_process_count"
-PROCESS_COUNT = "process_count"
-MEMORY_USAGE = "memory_usage"
-CPU_USAGE = "cpu_usage"
-EAT_MOST_MEMORY = "eat_most_memory"
-EAT_MOST_CPU = "eat_most_cpu"
-
-REPORT = {
-    USER: list(),
-    USERS_PROCESS_COUNT: defaultdict(int),
-    PROCESS_COUNT: 0,
-    MEMORY_USAGE: 0,
-    CPU_USAGE: 0,
-    EAT_MOST_CPU: "",
-    EAT_MOST_MEMORY: ""
-}
+from datetime import datetime
+from collections import defaultdict
 
 
 def getProcessData():
+    """Call in terminal command ps aux to get process list"""
     process = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE, universal_newlines=True).stdout.readlines()
     nfields = len(process[0].split()) - 1
     retval = []
@@ -33,10 +14,12 @@ def getProcessData():
 
 
 def get_count_of_process(lines):
+    """Get count of processes"""
     return len(lines)
 
 
 def get_users_from_process(lines):
+    """Taking all users which are in the process"""
     users = []
     for line in lines:
         if line[0] not in users:
@@ -45,17 +28,8 @@ def get_users_from_process(lines):
     return users
 
 
-def calculate_all_memory_usage(lines):
-    result = 0
-    for line in lines:
-        number = float(line[2])
-
-        result += number
-
-    return round(result, 2)
-
-
 def user_process_count(lines):
+    """Taking how much process have each user from the process"""
     process_by_user = defaultdict(int)
     for line in lines:
         user_item = line[0]
@@ -63,18 +37,53 @@ def user_process_count(lines):
     return process_by_user
 
 
-data = getProcessData()
+def calculate_memory_and_cpu_usage(lines):
+    """Counting how much CPU and MEMORY is using"""
+    memory_result = 0
+    cpu_result = 0
+    for line in lines:
+        memory_number = float(line[3])
+        cpu_number = float(line[2])
+        memory_result += memory_number
+        cpu_result += cpu_number
 
-calculated_memory_usage = calculate_all_memory_usage(data)
+    return (round(memory_result, 2), round(cpu_result, 2))
+
+
+def process_who_eat_cpu_and_memory(lines):
+    """Taking name of drained CPU and MEMORY process"""
+    highest_memory = 0
+    highest_cpu_load = 0
+    highest_memory_name = ""
+    highest_cpu_load_name = ""
+    for line in lines:
+        if float(line[3]) > highest_memory:
+            highest_memory = float(line[3])
+            highest_memory_name = line[10][:23]
+        elif float(line[2]) > highest_cpu_load:
+            highest_cpu_load = float(line[2])
+            highest_cpu_load_name = line[10][:23]
+
+    return (highest_memory_name, highest_cpu_load_name)
+
+
+data = getProcessData()
+cpu_and_memory = calculate_memory_and_cpu_usage(data)
 process_count = get_count_of_process(data)
 users = get_users_from_process(data)
 user_processes_count = user_process_count(data)
+name_of_highest_cpu_and_memory = process_who_eat_cpu_and_memory(data)
 
-REPORT[USER] = users
-REPORT[PROCESS_COUNT] = process_count
-REPORT[USERS_PROCESS_COUNT] = user_processes_count
+report = [
+    f"USERS IN PROCESS: {users}\n",
+    f"PROCESSES RUNNING: {process_count}\n",
+    f"PROCESSES BY USERS: {dict(user_processes_count)}\n",
+    f"TOTAL MEMORY USED: {cpu_and_memory[0]}\n",
+    f"TOTAL CPU USED: {cpu_and_memory[1]}\n",
+    f"PROCESS WITH MOST OF MEMORY USAGE: {name_of_highest_cpu_and_memory[0]}\n",
+    f"PROCESS WITH MOST OF CPU USAGE: {name_of_highest_cpu_and_memory[1]}",
 
-pprint.pprint(REPORT)
+]
 
-# with open('process_statistics.json', 'w') as fp:
-#     json.dump(REPORT, fp, indent=3)
+with open(f"{datetime.today():%d-%m-%Y-%H:%M}-scan.txt", 'w') as fp:
+    fp.writelines(report)
